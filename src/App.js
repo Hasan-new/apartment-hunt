fe666cedd0f2180068799a2d265a7e957fc69123
Maruf Hossain <hossain815265@gmail.com>
2020-11-16 00:24:27 +0600

    booking list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import BookingList from './components/BookingList/BookingList';
 import Home from './components/Home/Home';
 
 function App() {
   return (
     <div>
-      <Home></Home>
+      {/* <Home></Home> */}
+      <BookingList></BookingList>
     </div>
   );
 }
