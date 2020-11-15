857624bd31d6de4ac3ec1d7d8033ff0c892171e4
masudrana08 <rana028511@gmail.com>
2020-11-15 21:03:55 +0600

    sass dependency change

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,29 @@
 import React from 'react';
-
+import { Button, Nav, Navbar } from 'react-bootstrap';
+import './Header.css'
 const Header = () => {
    return (
       <div>
-         header
+         {/* bg="dark" variant="dark" */}
+         <Navbar collapseOnSelect expand="lg"  className="d-flex justify-content-between font-weight-bold">
+            <div>
+               <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>   
+            </div>
+            <div>
+               <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                  <Navbar.Collapse id="responsive-navbar-nav">
+                     <Nav>
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#about">About</Nav.Link>
+                        <Nav.Link href="#service">Service</Nav.Link>
+                        <Nav.Link href="#concern">Concern</Nav.Link>
+                        <Nav.Link href="#event">Event</Nav.Link>
+                        <Nav.Link href="#contact">Contact</Nav.Link>
+                        <Button className="btn btn-sm py-0 m-1">Login</Button>
+                     </Nav>
+                  </Navbar.Collapse>
+            </div>
+         </Navbar>
       </div>
    );
 };
