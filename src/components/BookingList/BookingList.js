c2c9c1bd2b974a42a22ce0377a83d0e83ddd5df7
Maruf Hossain <hossain815265@gmail.com>
2020-11-16 01:13:14 +0600

    Booking List Done

--- src/components/BookingList/BookingList.js
+++ src/components/BookingList/BookingList.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './BookingList.css';
-import logo from '../../images/logos/Logo.png'
+import logo from '../../images/logos/Logo.png';
+import booking_photo from '../../images/logos/map-marker-alt-solid 1.png'
+import add_house from '../../images/logos/plus 1.png'
+import my_rent from '../../images/logos/Rent.png'
 
 
 const BookingList = () => {
@@ -13,36 +16,52 @@ const BookingList = () => {
                     {/* </Link> */}
                     <div className="user-page-link">
                         {/* <Link className="userPageLink orderLink color-active" to='/admin/service-list'> */}
-                            <div className="d-flex justify-content-left order align-items-center">
-                                <p className="mx-0 my-2 pl-2">Service list</p>
+                            <div className="d-flex justify-content-left align-items-center active_link my-3">
+                                <img className="dashboard_logo" src={booking_photo} alt=""/>
+                                <p className="mx-0 my-0 ml-2">Booking list</p>
                             </div>
                         {/* </Link> */}
                         {/* <Link className="serviceListLink inactive-color" to="/admin/add-service"> */}
-                            <div className="d-flex justify-content-left serviceList align-items-center">
-                                <p className="mx-0 my-2 pl-2">Add Service</p>
+                            <div className="d-flex justify-content-left dashboard_link align-items-center my-3">
+                                <img className="dashboard_logo" src={add_house} alt=""/>
+                                <p className="mx-0 my-0 ml-2">Add House</p>
                             </div>
                         {/* </Link> */}
                         {/* <Link className="reviewLink inactive-color" to="/admin/add-admin"> */}
-                            <div className="d-flex justify-content-left review align-items-center">
-                                <p className="mx-0 my-2 pl-2">Add Admin</p>
+                            <div className="d-flex justify-content-left dashboard_link align-items-center my-3">
+                                <img className="dashboard_logo" src={my_rent} alt=""/>
+                                <p className="mx-0 my-0 ml-2">My Rent</p>
                             </div>
                         {/* </Link> */}
                     </div>
                 </div>
                 <div className="userPageRightSide">
-                    <h3 className="rightSideListText">Service list</h3>
-                    <div className="rightSideMain row pl-0">
+                    <h3 className="rightSideListText">Booking list</h3>
+                    <div className="rightSideMain row">
                         <table>
                             <thead>
                                 <tr>
-                                    <th style={{padding: '10px 10px 10px 20px', borderRadius: '10px 0 0 10px', width: '150px'}}>Name</th>
+                                    <th style={{padding: '10px 10px 10px 20px', borderRadius: '10px 0 0 10px', width: '190px'}}>Name</th>
                                     <th style={{width: '240px'}}>Email ID</th>
-                                    <th style={{width: '185px'}}>Service</th>
-                                    <th>ProjectDetails</th>
+                                    <th style={{width: '130px'}}>Phone No</th>
+                                    <th>Message</th>
                                     <th style={{borderRadius: '0 10px 10px 0', width: '110px'}}>Status</th>
                                 </tr>
                             </thead>
                             <tbody>
+                            <tr>
+                                <td className="user_name">MD. Maruf Hossain</td>
+                                <td>hossain815265@gmail.com</td>
+                                <td>01993980666</td>
+                                <td>I want to rent the house. Please contact me.</td>
+                                <td>
+                                    <select>
+                                        <option className="text-danger" value="Pending">Pending</option>
+                                        <option className="text-warning" value="On going">On going</option>
+                                        <option className="text-success" value="Done">Done</option>
+                                    </select>
+                                </td>
+                            </tr>
                                 {/* {
                                     orderedData.length === 0 && <button className="btn btn-primary m-auto" type="button" disabled>
                                     <span className="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
