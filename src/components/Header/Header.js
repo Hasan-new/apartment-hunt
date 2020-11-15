e037d9087bcd70522e14d70931d461bb0e6bfe8c
masudrana08 <rana028511@gmail.com>
2020-11-15 22:49:43 +0600

    header design done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,24 +5,20 @@ const Header = () => {
    return (
       <div>
          {/* bg="dark" variant="dark" */}
-         <Navbar collapseOnSelect expand="lg"  className="d-flex justify-content-between font-weight-bold">
-            <div>
+         <Navbar collapseOnSelect expand="lg"  className="font-weight-bold">
                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>   
-            </div>
-            <div>
                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                   <Navbar.Collapse id="responsive-navbar-nav">
-                     <Nav>
+                     <Nav className="text-center ml-auto">
                         <Nav.Link href="#home">Home</Nav.Link>
                         <Nav.Link href="#about">About</Nav.Link>
                         <Nav.Link href="#service">Service</Nav.Link>
                         <Nav.Link href="#concern">Concern</Nav.Link>
                         <Nav.Link href="#event">Event</Nav.Link>
                         <Nav.Link href="#contact">Contact</Nav.Link>
-                        <Button className="btn btn-sm py-0 m-1">Login</Button>
+                        <Button className=" py-0 m-1 login-btn">Login</Button>
                      </Nav>
                   </Navbar.Collapse>
-            </div>
          </Navbar>
       </div>
    );
