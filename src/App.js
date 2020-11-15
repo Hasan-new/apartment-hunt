92b89fb42e18074b36f64e9835bdc0c49fe7ec57
masudrana08 <rana028511@gmail.com>
2020-11-15 19:59:40 +0600

    header folder added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import './App.css';
+import Home from './components/Home/Home';
 
 function App() {
   return (
-    <div className="inverse">
-<<<<<<< HEAD
-      <h1>Welcome to Appoinment Hunts</h1>
-=======
-      <h1>Welcome to Appoinment Hunt</h1>
-      <h1>Maruf branch created</h1>
->>>>>>> 1b21495b7a7f9b031d9be74dd6712407b5cb1180
+    <div>
+      <Home></Home>
     </div>
   );
 }
