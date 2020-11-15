fd16382cc8a57a47e4bb9ec8e07f17171ce141bb
masudrana08 <rana028511@gmail.com>
2020-11-15 19:17:40 +0600

    image compressed

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="inverse">
-      <h1>Welcome to Appoinment Hunt</h1>
+      <h1>Welcome to Appoinment Hunts</h1>
     </div>
   );
 }
