a23f6091ca6faff2ee88bfdccc3ff94808ac41c1
Maruf Hossain <hossain815265@gmail.com>
2020-11-15 21:08:57 +0600

    done

--- src/App.js
+++ src/App.js
@@ -3,12 +3,8 @@ import './App.css';
 function App() {
   return (
     <div className="inverse">
-<<<<<<< HEAD
-      <h1>Welcome to Appoinment Hunts</h1>
-=======
       <h1>Welcome to Appoinment Hunt</h1>
       <h1>Maruf branch created</h1>
->>>>>>> 1b21495b7a7f9b031d9be74dd6712407b5cb1180
     </div>
   );
 }
