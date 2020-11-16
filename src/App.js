973476bca14cd27eb09afece4be52398d2194e9a
masudrana08 <rana028511@gmail.com>
2020-11-16 17:05:23 +0600

    apartment page design done

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './components/Home/Home';
 import MyRent from './components/MyRent/MyRent';
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Apartment from './components/Apartment/Apartment';
 function App() {
   return (
     <div>
@@ -30,6 +31,10 @@ function App() {
 
           <Route path="/my-rent">
             <MyRent></MyRent>
+          </Route>
+
+          <Route path="/apartment/:id">
+            <Apartment></Apartment>
           </Route>
 
         </Switch>
